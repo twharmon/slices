0c25d257957f97e1ce674d32f791609345bd7703
Travis Harmon <harmtrav@gmail.com>
2022-07-08 13:31:20 -0500

    Add Distinct method

--- slices_test.go
+++ slices_test.go
@@ -10,8 +10,8 @@ import (
 	"github.com/twharmon/slices"
 )
 
-func assertEqual(t *testing.T, got, want interface{}) {
-	if !reflect.DeepEqual(got, want) {
+func assertEqual(t *testing.T, want, got interface{}) {
+	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("want %v; got %v", want, got)
 	}
 }
@@ -20,7 +20,7 @@ func TestPush(t *testing.T) {
 	s := []int{1}
 	got := slices.Append(s, 5)
 	want := []int{1, 5}
-	assertEqual(t, got, want)
+	assertEqual(t, want, got)
 }
 
 func TestContains(t *testing.T) {
@@ -28,12 +28,12 @@ func TestContains(t *testing.T) {
 	t.Run("true", func(t *testing.T) {
 		got := slices.Contains(s, 2)
 		want := true
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("false", func(t *testing.T) {
 		got := slices.Contains(s, 4)
 		want := false
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -42,12 +42,12 @@ func TestSplice(t *testing.T) {
 	t.Run("with inserts", func(t *testing.T) {
 		got := slices.Splice(s, 1, 1, "boo")
 		want := []string{"foo", "boo", "baz"}
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("no inserts", func(t *testing.T) {
 		got := slices.Splice(s, 1, 1)
 		want := []string{"foo", "baz"}
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -56,13 +56,13 @@ func TestReverse(t *testing.T) {
 		s := []string{"foo", "bar"}
 		got := slices.Reverse(s)
 		want := []string{"bar", "foo"}
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("odd", func(t *testing.T) {
 		s := []string{"foo", "bar", "baz"}
 		got := slices.Reverse(s)
 		want := []string{"baz", "bar", "foo"}
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -70,7 +70,7 @@ func TestUnshift(t *testing.T) {
 	s := []string{"foo"}
 	got := slices.Unshift(s, "bar")
 	want := []string{"bar", "foo"}
-	assertEqual(t, got, want)
+	assertEqual(t, want, got)
 }
 
 func TestFind(t *testing.T) {
@@ -80,14 +80,14 @@ func TestFind(t *testing.T) {
 			return item == "foo"
 		})
 		want := "foo"
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("not found", func(t *testing.T) {
 		got := slices.Find(s, func(item string) bool {
 			return item == "bar"
 		})
 		want := ""
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -96,12 +96,12 @@ func TestIndexOf(t *testing.T) {
 	t.Run("found", func(t *testing.T) {
 		got := slices.IndexOf(s, "foo")
 		want := 0
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("not found", func(t *testing.T) {
 		got := slices.IndexOf(s, "bar")
 		want := -1
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -110,12 +110,12 @@ func TestIndexOfFunc(t *testing.T) {
 	t.Run("found", func(t *testing.T) {
 		got := slices.IndexOfFunc(s, func(item string) bool { return item == "foo" })
 		want := 0
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("not found", func(t *testing.T) {
 		got := slices.IndexOfFunc(s, func(item string) bool { return item == "bar" })
 		want := -1
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -132,13 +132,13 @@ func TestMax(t *testing.T) {
 		s := []int{2, 6, 1, 4, 3}
 		got := slices.Max(s)
 		want := 6
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("zero value", func(t *testing.T) {
 		s := []string{}
 		got := slices.Max(s)
 		want := ""
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -147,13 +147,13 @@ func TestMin(t *testing.T) {
 		s := []int{2, 6, 1, 4, 3}
 		got := slices.Min(s)
 		want := 1
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("zero value", func(t *testing.T) {
 		s := []string{}
 		got := slices.Min(s)
 		want := ""
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -162,13 +162,13 @@ func TestMaxFunc(t *testing.T) {
 		s := []int{2, 6, 1, 4, 3}
 		got := slices.MaxFunc(s, func(a, b int) bool { return a < b })
 		want := 6
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("zero value", func(t *testing.T) {
 		s := []int{}
 		got := slices.MaxFunc(s, func(a, b int) bool { return a < b })
 		want := 0
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -177,13 +177,13 @@ func TestMinFunc(t *testing.T) {
 		s := []int{2, 6, 1, 4, 3}
 		got := slices.MinFunc(s, func(a, b int) bool { return a < b })
 		want := 1
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("zero value", func(t *testing.T) {
 		s := []int{}
 		got := slices.MinFunc(s, func(a, b int) bool { return a < b })
 		want := 0
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -223,7 +223,7 @@ func TestMap(t *testing.T) {
 		return len(i)
 	})
 	want := []int{1, 2, 3}
-	assertEqual(t, got, want)
+	assertEqual(t, want, got)
 }
 
 func TestReduce(t *testing.T) {
@@ -232,7 +232,7 @@ func TestReduce(t *testing.T) {
 		return cnt + len(i)
 	})
 	want := 6
-	assertEqual(t, got, want)
+	assertEqual(t, want, got)
 }
 
 func TestSortFunc(t *testing.T) {
@@ -247,7 +247,7 @@ func TestSortFunc(t *testing.T) {
 	sort.Slice(want, func(i, j int) bool {
 		return want[i] < want[j]
 	})
-	assertEqual(t, got, want)
+	assertEqual(t, want, got)
 }
 
 func TestSort(t *testing.T) {
@@ -258,7 +258,7 @@ func TestSort(t *testing.T) {
 	got := slices.Sort(s)
 	want := slices.Clone(s)
 	sort.Ints(want)
-	assertEqual(t, got, want)
+	assertEqual(t, want, got)
 }
 
 func TestIntersection(t *testing.T) {
@@ -267,7 +267,7 @@ func TestIntersection(t *testing.T) {
 		b := []string{"bar", "baz"}
 		want := []string{"bar"}
 		got := slices.Intersection(a, b)
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("empty slice", func(t *testing.T) {
 		var s [][]string
@@ -279,7 +279,7 @@ func TestIntersection(t *testing.T) {
 		b := []string{"baz"}
 		want := []string{}
 		got := slices.Intersection(a, b)
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("dupes in slice", func(t *testing.T) {
 		a := []string{"foo", "bar", "baz", "foo"}
@@ -287,7 +287,7 @@ func TestIntersection(t *testing.T) {
 		c := []string{"foo", "foo", "foo"}
 		want := []string{}
 		got := slices.Sort(slices.Intersection(a, b, c))
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 }
 
@@ -297,11 +297,25 @@ func TestUnion(t *testing.T) {
 		b := []string{"bar", "baz"}
 		want := slices.Sort([]string{"foo", "bar", "baz"})
 		got := slices.Sort(slices.Union(a, b))
-		assertEqual(t, got, want)
+		assertEqual(t, want, got)
 	})
 	t.Run("empty slice", func(t *testing.T) {
 		var s [][]string
 		got := slices.Union(s...)
+		assertEqual(t, got, []string{})
+	})
+}
+
+func TestDistinct(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		a := []string{"foo", "bar", "baz", "foo"}
+		want := slices.Sort([]string{"foo", "bar", "baz"})
+		got := slices.Sort(slices.Distinct(a))
+		assertEqual(t, want, got)
+	})
+	t.Run("empty slice", func(t *testing.T) {
+		var s []string
+		got := slices.Distinct(s)
 		assertEqual(t, got, []string{})
 	})
 }
